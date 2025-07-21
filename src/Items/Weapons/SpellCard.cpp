06efda33bf32d05a5b448c6834294ddd74e02426
Zhengyang Zhao <1195664999@qq.com>
2025-07-21 21:36:15 +0800

    Update SpellCard image path and BattleScene weapon
    Corrected the image path for SpellCard and changed the default spare weapon in BattleScene from Ranged to SpellCard for initial scene setup.

--- src/Items/Weapons/SpellCard.cpp
+++ src/Items/Weapons/SpellCard.cpp
@@ -1,6 +1,6 @@
 #include "SpellCard.h"
 
-SpellCard::SpellCard(QGraphicsItem *parent) : Weapon(parent, ":/Items/Weapons/5.png") {
+SpellCard::SpellCard(QGraphicsItem *parent) : Weapon(parent, ":/Weapons/Items/Weapons/5.png") {
     weaponID = 5; // 设置符卡的ID
     ammoCapacity = 5; // 设置弹药容量
 }
