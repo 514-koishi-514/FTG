3b78439e5cea8026ebf3e18f8d8cde7364bcd8ae
Zhengyang Zhao <1195664999@qq.com>
2025-07-22 17:56:23 +0800

    Update HeavyArmor.cpp

--- src/Items/Armors/HeavyArmor.cpp
+++ src/Items/Armors/HeavyArmor.cpp
@@ -3,7 +3,7 @@
 HeavyArmor::HeavyArmor(QGraphicsItem *parent) : Armor(parent,
                                                        ":/Items/Armors/HeavyArmor/HeavyArmor.png") {
     // 重型护甲的构造函数
-    armorValue = 100; // 设置重型护甲值
-    maxArmorValue = 100; // 设置重型护甲的最大值
+    armorValue = 50; // 设置重型护甲值
+    maxArmorValue = 50; // 设置重型护甲的最大值
     armorType = Heavy; // 设置护甲类型为重型护甲
 }
